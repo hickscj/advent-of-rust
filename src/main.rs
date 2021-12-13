b9e15e8a759b8fb07bb622589b180e2936032794
Chad Hicks <chadjhicks@gmail.com>
2021-12-12 22:09:20 -0500

    printing things

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,43 @@
 // use std::env;
-use std::fs;
+// use std::fs;
 
 fn main() {
-    let contents: String = fs::read_to_string("input.txt")
-        .expect("Something went wrong");
-
-    let mut x: i32 = 0;
-    for num in contents.chars() {
-        println!("{}", num);
-        x = x + 1;
-    }
+    print_examples();
 }
 
-fn test_function() {
-    println!("This is the test function");
+fn print_examples() {
+    /*
+     * https://doc.rust-lang.org/rust-by-example/hello/print.html 
+     */
+    println!("---------------------");
+    println!("Examples of printing statement formats in Rust");
+    println!("---------------------");
+    println!("{} days", 31);
+
+    // variable interpolation
+    println!("{subject} {verb} {object}",
+        object="the lazy dog",
+        subject="the quick brown fox",
+        verb="jumps over");
+    
+    // special formatting
+    println!("{} of {:b} people know binary", 1, 2);
+
+    // right-align text
+    println!("{number:>width$}", number=1, width=16);
+
+    // pad numbers with extra zeroes
+    println!("{number:0>width$}", number=1, width=6);
+
+    // create a structure named `Structure` which contains an i32
+    #[derive(Debug)]
+    struct Structure {
+        id: i32
+    }
+
+    let a_struct = Structure { id: 12 };
+
+    // custom types require more complex handling
+    // println!("This struct `{}` won't print...", Structure(3));
+     println!("This struct {:?} WILL", a_struct.id);
 }
